7c452880af1cf9b89d12286d097a5bbc1f4639b7
Chiliseo <adrianochiliseo@hotmail.com>
2015-05-06 17:03:07 -0600

    upload blog

--- app.js
+++ app.js
@@ -4,4 +4,7 @@ app.set('port', process.env.PORT || 3000);
 app.get('/',function (req,res) {
 	res.send("Hola Adriano")
 });
+app.get('/blog',function (req,res) {
+	res.redirect('./blog/');
+});
 app.listen(app.get('port'));
