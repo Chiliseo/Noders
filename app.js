3bb9a3450c63eed9ea50326fb27bd98589045ec9
Chiliseo <adrianochiliseo@hotmail.com>
2015-05-06 17:33:50 -0600

    app routes blog comments

--- app.js
+++ app.js
@@ -1,15 +1,16 @@
 var express = require('express');
-var ghost = require('./blog/core/');
+var ghost = require('ghost');
+
 var app = express();
 app.set('port', process.env.PORT || 3000);
 app.get('/',function (req,res) {
 	res.send("Hola Adriano")
 });
-var blog = ghost({
-    contentDirectory: "./blog/content"
-});
+// var blog = ghost({
+//     contentDirectory: "./blog/content"
+// });
 
-app.use(blog.middleware({
-    urlRoot: "/blog"
-}));
+// app.use(blog.middleware({
+//     urlRoot: "/blog"
+// }));
 app.listen(app.get('port'));
