845c5a7f3bfa4bff759d10e8c4df179f1f03598d
Chiliseo <adrianochiliseo@hotmail.com>
2015-05-06 17:32:29 -0600

    upload blog

--- app.js
+++ app.js
@@ -1,10 +1,15 @@
 var express = require('express');
+var ghost = require('./blog/core/');
 var app = express();
 app.set('port', process.env.PORT || 3000);
 app.get('/',function (req,res) {
 	res.send("Hola Adriano")
 });
-app.get('/blog',function (req,res) {
-	res.redirect('./blog/');
+var blog = ghost({
+    contentDirectory: "./blog/content"
 });
+
+app.use(blog.middleware({
+    urlRoot: "/blog"
+}));
 app.listen(app.get('port'));
