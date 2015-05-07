6cbc211eabf8078ac463cc2a09845fd34f9c29f9
Chiliseo <adrianochiliseo@hotmail.com>
2015-05-07 15:27:46 -0600

    delelte blog

--- app.js
+++ app.js
@@ -1,16 +1,7 @@
 var express = require('express');
-var ghost = require('ghost');
-
 var app = express();
 app.set('port', process.env.PORT || 3000);
 app.get('/',function (req,res) {
 	res.send("Hola Adriano")
 });
-// var blog = ghost({
-//     contentDirectory: "./blog/content"
-// });
-
-// app.use(blog.middleware({
-//     urlRoot: "/blog"
-// }));
 app.listen(app.get('port'));
